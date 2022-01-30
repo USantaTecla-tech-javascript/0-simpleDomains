1a87dc1d48e235a07d9415fb98a2889577711f31
Setillo <setillofm@gmail.com>
2022-01-30 20:31:07 +0100

    refactor

---
+++ 6-collections/5-includes/v0.7/app.js
@@ -0,0 +1,36 @@
+const { Console } = require("./console");
+
+let console = new Console();
+const key = console.readString(`Dame un valor para buscar en la serie: `);
+const data = readData();
+console.writeln(`El número ${includes(data, key)} se encuentra en la serie`);
+
+function readData(){
+  let oneData = readOneData();
+  return oneData === "." ?
+      []
+    :
+      [oneData].concat(readData());
+
+  function readOneData(){
+    return console.readString(`Dame un valor (. para finalizar): `);
+  }
+}
+
+function includes(list, key){
+  const YES = `${key} si`;
+  const NO = `${key} no`;
+
+  if (list === [] || list[0] === `.`){
+    return NO;
+  } else {
+    let [head, ...tail] = list;
+    if (head == `.`){
+      return YES;
+    }
+    if (tail.length === 0){
+      return NO;
+    }
+    return includes(tail, key);
+  }
+}
