57cd3c0935fba5d8558c1cf660c1226a157905a0
Setillo <setillofm@gmail.com>
2022-02-13 19:29:07 +0100

    refactor

--- 3-numbers/5-changeCoins/v1.0/app.js
+++ 3-numbers/5-changeCoins/v1.0/app.js
@@ -8,30 +8,30 @@ const amount50 = amount - remainder50;
 const coins50 = amount50 / 50;
 let msg = coins50 > 0 ? `${coins50} moneda(s) de 50\n` : ``;
 
-amount = amount + amount50;
+amount = amount - amount50;
 const remainder20 = amount % 20;
 const amount20 = amount - remainder20;
 const coins20 = amount20 / 20;
 msg += coins20 > 0 ? `${coins20} moneda(s) de 20\n` : ``;
 
-amount = amount + amount20;
+amount = amount - amount20;
 const remainder10 = amount % 10;
 const amount10 = amount - remainder10;
 const coins10 = amount10 / 10;
 msg += coins10 > 0 ? `${coins10} moneda(s) de 10\n` : ``;
 
-amount = amount + amount10;
+amount = amount - amount10;
 const remainder5 = amount % 5;
 const amount5 = amount - remainder5;
 const coins5 = amount5 / 5;
 msg += coins5 > 0 ? `${coins5} moneda(s) de 5\n` : ``;
 
-amount = amount + amount5;
+amount = amount - amount5;
 const remainder2 = amount % 2;
 const amount2 = amount - remainder2;
 const coins2 = amount2 / 2;
 msg += coins2 > 0 ? `${coins2} moneda(s) de 2\n` : ``;
 
-amount = amount + amount2;
+amount = amount - amount2;
 msg += amount > 0 ? `${amount} moneda(s) de 1\n` : ``;
-console.writeln(msg50 + msg20 + msg10 + msg5 + msg2 + msg1);
+console.writeln(msg);
