82e30eef9ac983aaba74a8d7e29ed8dfa9804862
Setillo <setillofm@gmail.com>
2022-02-13 20:07:30 +0100

    refactor

--- 3-numbers/2-percentage/v0.0/app.js
+++ 3-numbers/2-percentage/v0.0/app.js
@@ -2,6 +2,6 @@ const { Console } = require("./console");
 
 const console = new Console();
 const percentage = console.readNumber(`Dame el tanto por ciento (sin %): `) ;
-console.writeln(`${percentage}% = ${percentage} · 1 / 100 = ${percentage} · 0,01 = ${percentage/100}`);
+console.writeln(`${percentage}% = ${percentage} x 1 / 100 = ${percentage} x 0.01 = ${percentage/100}`);
 const amount = console.readNumber("Dame la cantidad: ");
-console.writeln(`${percentage}% · ${amount} = ${amount * percentage / 100}`);
+console.writeln(`${percentage}% x ${amount} = ${amount * percentage / 100}`);
