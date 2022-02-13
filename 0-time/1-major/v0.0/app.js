7571cf3afcc5d563de539891b250076fc3d4b39b
Setillo <setillofm@gmail.com>
2022-02-13 17:29:33 +0100

    refactor

--- 0-time/1-major/v0.0/app.js
+++ 0-time/1-major/v0.0/app.js
@@ -1,5 +1,5 @@
 const { Console } = require("./console");
 
 const console = new Console();
-let age = console.readNumber("Qué años tienes? ");
+const age = console.readNumber("Qué años tienes? ");
 console.writeln(`${age >= 18? `Sí` : `No`} eres mayor de edad`);
