3628c5d27fa410b800c3b176809a3567b8809832
Setillo <setillofm@gmail.com>
2022-02-13 21:21:20 +0100

    refactor

--- 3-numbers/0-multiplicationTable/v0.4/app.js
+++ 3-numbers/0-multiplicationTable/v0.4/app.js
@@ -2,6 +2,6 @@ const { Console } = require("./console");
 
 const console = new Console();
 const number = console.readNumber("Dame un número: ");
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) //{
   console.writeln(`${number} * ${i} = ${number * i}`);
-}
+//}
