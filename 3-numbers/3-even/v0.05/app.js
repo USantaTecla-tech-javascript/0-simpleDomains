42512b67e5c6254ac33c0956db700d9b2cbcd76e
Setillo <setillofm@gmail.com>
2022-02-13 20:15:59 +0100

    refactor

--- 3-numbers/3-even/v0.05/app.js
+++ 3-numbers/3-even/v0.05/app.js
@@ -2,4 +2,4 @@ const { Console } = require("./console");
 
 const console = new Console();
 const number = console.readNumber("Escribe un número? ");
-console.writeln(`El numero ${number} es ${number % 2 == 0 ? "par" : "impar"}`);
+console.writeln(`El numero ${number} es ${number % 2 === 0 ? "par" : "impar"}`);
