4edb3fd60e4ba650771b74f33c454ad40d7be7ac
Setillo <setillofm@gmail.com>
2022-02-13 15:29:04 +0100

    refactor

--- 3-numbers/5-changeCoins/v0.0/app.js
+++ 3-numbers/5-changeCoins/v0.0/app.js
@@ -2,27 +2,26 @@ const { Console } = require("./console");
 
 const console = new Console();
 let amount = console.readNumber("Dame la cantidad de céntimos: ");
-
 let amount50 = amount - amount % 50;
-amount = amount - amount50;
 let coins50 = amount50 / 50;
 
+amount = amount - amount50;
 let amount20 = amount - amount % 20;
-amount = amount - amount20;
 let coins20 = amount20 / 20;
 
+amount = amount - amount20;
 let amount10 = amount - amount % 10;
-amount = amount - amount10;
 let coins10 = amount10 / 10;
 
+amount = amount - amount10;
 let amount5 = amount - amount % 5;
-amount = amount - amount5;
 let coins5 = amount5 / 5;
 
+amount = amount - amount5;
 let amount2 = amount - amount % 2;
-amount = amount - amount2;
 let coins2 = amount2 / 2;
 
+amount = amount - amount2;
 console.writeln(`${coins50} moneda(s) de 50
 ${coins20} moneda(s) de 20 
 ${coins10} moneda(s) de 10 
