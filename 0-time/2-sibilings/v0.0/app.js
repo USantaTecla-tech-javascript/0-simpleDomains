31996e856014a4ce17f89d081c5d445a8edf0795
Setillo <setillofm@gmail.com>
2022-02-13 20:04:33 +0100

    refactor

--- 0-time/2-sibilings/v0.0/app.js
+++ 0-time/2-sibilings/v0.0/app.js
@@ -4,6 +4,7 @@ const console = new Console();
 const age = console.readNumber("Dame tu edad: ");
 const previous = age - 1;
 const next = age + 1;
-let prefix = `E` + age === 0 ? `` : `l año pasado tenías ${previous} año${previous !== 1 ? `s` : ``}
-y e`;
-console.writeln(prefix + `l año que viene tendrás ${next} año${next !== 1 ? `s` : ``}`);
+const prefix = `E`;
+const suffix = age === 0 ? `` : `l año pasado tenías ${previous} año${previous !== 1 ? `s` : ``}
+y e`; 
+console.writeln(prefix + suffix + `l año que viene tendrás ${next} año${next !== 1 ? `s` : ``}`);
