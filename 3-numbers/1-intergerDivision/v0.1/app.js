1b0de8aba96ab88401dc477e50f9ba9758727819
Setillo <setillofm@gmail.com>
2022-02-13 20:09:23 +0100

    refactor

--- 3-numbers/1-intergerDivision/v0.1/app.js
+++ 3-numbers/1-intergerDivision/v0.1/app.js
@@ -3,7 +3,7 @@ const { Console } = require("./console");
 const console = new Console();
 const dividend = console.readNumber(`Dame el dividendo: `);
 const divider = console.readNumber(`Dame el divisor: `);
-console.writeln(`${dividend} / ${divider} + = ${integerDivision(dividend, divider).quotient} y sobran ${integerDivision(dividend, divider).remainder}`);
+console.writeln(`${dividend} / ${divider} = ${integerDivision(dividend, divider).quotient} y sobran ${integerDivision(dividend, divider).remainder}`);
 
 function integerDivision(dividend, divider){
   let integer = dividend - dividend % divider;
--- 3-numbers/1-intergerDivision/v1.0/app.js
+++ 3-numbers/1-intergerDivision/v1.0/app.js
@@ -6,5 +6,5 @@ for (let i = 0; i <= amount; i++) {
   const remainder = i % divider;
   const integer = i - remainder;
   const quotient = integer / divider;
-  console.writeln(`${i} / ${divider} + = ${quotient} y sobran ${remainder}`);
+  console.writeln(`${i} / ${divider} = ${quotient} y sobran ${remainder}`);
 }
