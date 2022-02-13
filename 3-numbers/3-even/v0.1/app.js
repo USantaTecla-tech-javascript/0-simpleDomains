b54bab515d35cd34c197352be0a5149ee0e29fef
Setillo <setillofm@gmail.com>
2022-02-13 19:52:51 +0100

    refactor

--- 3-numbers/3-even/v0.1/app.js
+++ 3-numbers/3-even/v0.1/app.js
@@ -3,5 +3,5 @@ const { Console } = require("./console");
 const console = new Console();
 const number = console.readNumber("Escribe un número? ");
 const firstBit = number & 1;
-const parity = firstBit == 0 ? "par" : "impar";
+const parity = firstBit === 0 ? "par" : "impar";
 console.writeln(`El numero ${number} es ${parity}`);
