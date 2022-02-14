e65e2939f508d9ca5997c09219704a32dd6bf62a
Setillo <setillofm@gmail.com>
2022-02-14 09:32:25 +0100

    refactor

---
+++ 4-numberingSystems/4-decimalToBinary/v0.2/app.js
@@ -0,0 +1,11 @@
+const { Console } = require("./console");
+
+const console = new Console();
+let decimalNumber = console.readNumber("Dame un numero? ")
+const SIZE_BITS = 32;
+let binaryNumber = ``;
+for(let i=0; i<SIZE_BITS; i++){
+	binaryNumber = (decimalNumber & 1) + binaryNumber;
+  decimalNumber >>= 1;
+}
+console.writeln(binaryNumber);
