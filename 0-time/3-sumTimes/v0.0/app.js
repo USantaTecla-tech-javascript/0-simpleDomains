a950f0afc9ec9ccb5c690f7f1726f6390a960beb
Setillo <setillofm@gmail.com>
2022-02-16 17:10:55 +0100

    refactor

--- 0-time/3-sumTimes/v0.0/app.js
+++ 0-time/3-sumTimes/v0.0/app.js
@@ -5,11 +5,11 @@ const hours = console.readNumber("Dime la hora actual [0-23]: ");
 const minutes = console.readNumber("Dime los minutos actuales [0-59]: ");
 const neededHours = console.readNumber("Dime cuantas horas necesitas [0-23]: ");
 const neededMinutes = console.readNumber("Dime los minutos necesitas [0-59]: ");
-const MINUTES_PER_HOUR = 60;
-let afterMinutes = minutes + neededMinutes;
 let afterHours = hours + neededHours;
+let afterMinutes = minutes + neededMinutes;
+const MINUTES_PER_HOUR = 60;
 if (afterMinutes >= MINUTES_PER_HOUR){
-  afterMinutes -= 60;
+  afterMinutes -= MINUTES_PER_HOUR;
   afterHours++;
 }
 console.writeln(`Serán las ${afterHours}:${afterMinutes}`);
