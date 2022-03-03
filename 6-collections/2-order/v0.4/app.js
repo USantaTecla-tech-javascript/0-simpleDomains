e4c98182cbaab5286a9d5b1fae0bba4e6aa71327
Setillo <setillofm@gmail.com>
2022-03-03 09:54:47 +0100

    refactor

--- 6-collections/2-order/v0.4/app.js
+++ 6-collections/2-order/v0.4/app.js
@@ -1,6 +1,10 @@
 const { Console } = require("./console");
 
 const console = new Console();
+let measures = [9,2,5,7,1,3,8,0,4,6];
+print("desordenados", measures);
+sort(measures);
+print("ordenados", measures);
 
 function print(title, data) {
   console.writeln(`Valores ${title} :`);
@@ -37,22 +41,4 @@ function sort(values){
   quicksort(values, 0, values.length);
 }
 
-function sort(values) {   // bubble sort
-  let change;
-  do {
-    change = false;
-    for (let i = 0; i < values.length - 1; i++) {
-      if (values[i] > values[i + 1]) {
-        let temp = values[i];
-        values[i] = values[i + 1];
-        values[i + 1] = temp;
-        change = true;
-      }
-    }
-  } while (change);
-}
 
-let measures = [9,2,5,7,1,3,8,0,4,6];
-print("desordenados", measures);
-sort(measures);
-print("ordenados", measures);
