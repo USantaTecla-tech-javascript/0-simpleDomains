964a264a6fca41a2eb59e396b558b8996ea2454c
Setillo <setillofm@gmail.com>
2022-09-26 18:16:51 +0200

    refactor

--- 2-texts/5-vowels/v0.0/app.js
+++
@@ -1,9 +0,0 @@
-const { Console } = require("./console");
-
-const console = new Console();
-let input = console.readString(`¿Dame un mensaje terminado en "."?: `);
-let characters = 0;
-while(input[characters] !== `.`) //{
-  characters++;
-//}
-console.writeln(`Tu mensaje "${input}" tiene ${characters} caracteres significativos, sin contar el punto final!`);
